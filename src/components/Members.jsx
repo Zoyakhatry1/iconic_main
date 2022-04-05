06b5f90cbf2d8328bbdf64af56b69390efa7867e
Zoyakhatry1 <83895340+Zoyakhatry1@users.noreply.github.com>
2022-04-05 14:26:38 +0530

    members

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -7,8 +7,85 @@ function Members() {
   return (
     <div className={classes.members}>
        <div className={classes.content}>
-       <h1>Chief Patron</h1>
-       <img src={dirimg} alt="" />
+       <h1 className={classes.heading}>Chief Patron</h1>
+       <img className={classes.dirimg} src={dirimg} alt="" />
+       <p className={classes.name}>- Prof. (Dr.) Rakesh Sehgal</p>
+       <p className={classes.name}>(Director, NIT Srinagar)</p>
+       <div className={classes.subcontent}>
+       <h1 className={classes.heading}>Patron</h1>
+       <p className={classes.name}>- Prof. (Dr.) Sunil Khanna</p>
+       <p className={classes.name}>(Director, SOT, PDEU)</p>
+       </div>
+       <div className={classes.subcontent}>
+         <h1 className={classes.heading}>Advisory Board</h1>
+         <p className={classes.name}>- Prof. (Dr.) Sunil Khanna</p>
+         <p className={classes.name}>(Director, SOT, PDEU)</p>
+         <p className={classes.name}>- Prof. (Dr.) Sunil Khanna</p>
+         <p className={classes.name}>(Director, SOT, PDEU)</p>
+         <p className={classes.name}>- Prof. (Dr.) Sunil Khanna</p>
+         <p className={classes.name}>(Director, SOT, PDEU)</p>
+       </div>
+       <div className={classes.subcontent}>
+         <h1 className={classes.heading}>General Chair</h1>
+         <p className={classes.name}>- Dr. Shakti Mishra (PDEU, Gandhinagar)</p>
+         <p className={classes.name}>- Dr. Shakti Mishra (PDEU, Gandhinagar)</p>
+         <p className={classes.name}>- Dr. Shakti Mishra (PDEU, Gandhinagar)</p>
+         <p className={classes.name}>- Dr. Shakti Mishra (PDEU, Gandhinagar)</p>
+       </div>
+       <div className={classes.subcontent}>
+        <h1 className={classes.heading}>Technical Chair(s)</h1>
+        <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+        <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+        <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+        <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       </div>
+       <div className={classes.subcontent}>
+         <h1 className={classes.heading}>Publication Chair(s)</h1>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       </div>
+
+       <div className={classes.subcontent}>
+        <h1 className={classes.heading}>Finance Chair</h1>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       </div>
+
+       <div className={classes.subcontent}>
+         <h1 className={classes.heading}>Industry Liasion Chair</h1>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       </div>
+
+       <div className={classes.subcontent}>
+         <h1 className={classes.heading}>Publicity Committee Chair(s)</h1>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       </div>
+
+       <div className={classes.subcontent}>
+       <h1 className={classes.heading}>Publicity Committee</h1>
+       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       </div>
+
+       <div className={classes.subcontent}>
+         <h1 className={classes.heading}>Special Session Chair(s)</h1>
+         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       </div>
        </div>
     </div>
   )
