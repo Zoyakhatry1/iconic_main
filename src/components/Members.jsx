80f34cc7a590143dd5851bb67a287ecea6a9b29a
Zoyakhatry1 <83895340+Zoyakhatry1@users.noreply.github.com>
2022-04-06 12:45:52 +0530

    changesinabout and members

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import classes from "../styles/Members.module.css";
 import dirimg from "../assets/director.jpg";
+import iitdirimg from "../assets/iit_director.png";
+import profimg from "../assets/prof.jpg";
 
 
 function Members() {
@@ -9,38 +11,61 @@ function Members() {
        <div className={classes.content}>
        <h1 className={classes.heading}>Chief Patron</h1>
        <img className={classes.dirimg} src={dirimg} alt="" />
-       <p className={classes.name}>- Prof. (Dr.) Rakesh Sehgal</p>
+       <p className={classes.name}>- Prof(Dr.) Rakesh Sehgal</p>
        <p className={classes.name}>(Director, NIT Srinagar)</p>
+
        <div className={classes.subcontent}>
        <h1 className={classes.heading}>Patron</h1>
-       <p className={classes.name}>- Prof. (Dr.) Sunil Khanna</p>
-       <p className={classes.name}>(Director, SOT, PDEU)</p>
+       <img className={classes.dirimg} src={iitdirimg} alt="" />
+       <p className={classes.name}>- Prof(Dr.) Manoj Singh Gaur</p>
+       <p className={classes.name}>(Director, IIT Jammu)</p>
        </div>
+
        <div className={classes.subcontent}>
-         <h1 className={classes.heading}>Advisory Board</h1>
-         <p className={classes.name}>- Prof. (Dr.) Sunil Khanna</p>
-         <p className={classes.name}>(Director, SOT, PDEU)</p>
-         <p className={classes.name}>- Prof. (Dr.) Sunil Khanna</p>
-         <p className={classes.name}>(Director, SOT, PDEU)</p>
-         <p className={classes.name}>- Prof. (Dr.) Sunil Khanna</p>
-         <p className={classes.name}>(Director, SOT, PDEU)</p>
+         <h1 className={classes.heading}>Conference Chair</h1>
+         <img className={classes.dirimg} src={profimg} alt="" />
+         <p className={classes.name}>- Prof. Roohie Naaz Mir</p>
+         <p className={classes.name}>(HOD, CSE , NIT Srinagar)</p>
        </div>
-       <div className={classes.subcontent}>
-         <h1 className={classes.heading}>General Chair</h1>
-         <p className={classes.name}>- Dr. Shakti Mishra (PDEU, Gandhinagar)</p>
-         <p className={classes.name}>- Dr. Shakti Mishra (PDEU, Gandhinagar)</p>
-         <p className={classes.name}>- Dr. Shakti Mishra (PDEU, Gandhinagar)</p>
-         <p className={classes.name}>- Dr. Shakti Mishra (PDEU, Gandhinagar)</p>
-       </div>
+
        <div className={classes.subcontent}>
         <h1 className={classes.heading}>Technical Chair(s)</h1>
-        <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-        <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-        <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-        <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+        <p className={classes.name}>- Dr. Shaima Qureshi</p>
+        <p className={classes.name}>(NIT Srinagar)</p>
+        <p className={classes.name}>- Dr. Dharitri Rath</p>
+        <p className={classes.name}>(IIT Jammu)</p>
+        <p className={classes.name}>- Dr. Badri N subudhi</p>
+        <p className={classes.name}>(IIT Jammu)</p>
        </div>
+
        <div className={classes.subcontent}>
          <h1 className={classes.heading}>Publication Chair(s)</h1>
+         <p className={classes.name}>- Dr. Ranjeet Kumar Rout</p>
+         <p className={classes.name}>(NIT Srinagar)</p>
+         <p className={classes.name}>- Dr. Vinit Jakhetiya</p>
+         <p className={classes.name}>(IIT Jammu)</p>
+         <p className={classes.name}>- Dr. Saiyed Umer</p>
+         <p className={classes.name}>(Aliah University, Kolkata)</p>
+       </div>
+
+       <div className={classes.subcontent}>
+        <h1 className={classes.heading}>Publicity Chair(s)</h1>
+         <p className={classes.name}>- Dr. Pramod Kumar Yadav</p>
+         <p className={classes.name}>(NIT Srinagar)</p>
+         <p className={classes.name}>- Dr. Veningston K</p>
+         <p className={classes.name}>(NIT Srinagar)</p>
+       </div>
+
+       <div className={classes.subcontent}>
+         <h1 className={classes.heading}>Organizing Secretary</h1>
+         <p className={classes.name}>- Dr. Sparsh Sharma</p>
+         <p className={classes.name}>- Dr. Saiyed Umer</p>
+         <p className={classes.name}>- Dr. Pramod Kumar Yadav</p>
+         <p className={classes.name}>- Dr. Veningston K</p>
+       </div>
+
+       <div className={classes.subcontent}>
+         <h1 className={classes.heading}>Organizing Committee</h1>
          <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
          <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
          <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
@@ -48,39 +73,15 @@ function Members() {
        </div>
 
        <div className={classes.subcontent}>
-        <h1 className={classes.heading}>Finance Chair</h1>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
+       <h1 className={classes.heading}>Finance Committee</h1>
+       <p className={classes.name}>- Dr. Sudhir</p>
+       <p className={classes.name}>- Dr. Ranjeet Kumar Rout</p>
+       <p className={classes.name}>- Dr. Saiyed Umer</p>
+       <p className={classes.name}>- Dr. Sparsh Sharma</p>
        </div>
 
        <div className={classes.subcontent}>
-         <h1 className={classes.heading}>Industry Liasion Chair</h1>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-       </div>
-
-       <div className={classes.subcontent}>
-         <h1 className={classes.heading}>Publicity Committee Chair(s)</h1>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-         <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-       </div>
-
-       <div className={classes.subcontent}>
-       <h1 className={classes.heading}>Publicity Committee</h1>
-       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-       <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
-       </div>
-
-       <div className={classes.subcontent}>
-         <h1 className={classes.heading}>Special Session Chair(s)</h1>
+         <h1 className={classes.heading}>Technical Programme Committee</h1>
          <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
        <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
        <p className={classes.name}>- Dr. Pankaj Kumar (DAIICT)</p>
