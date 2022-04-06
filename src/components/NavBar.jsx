3ddced4a4b723730e3ef4a74286b7c9b4b6f7c87
Zoyakhatry1 <83895340+Zoyakhatry1@users.noreply.github.com>
2022-04-06 14:54:57 +0530

    venue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -48,6 +48,11 @@ function NavBar() {
             </Link>
           </li>
           <li className="nav-item">
+            <Link to="/venue" className="nav-links" onClick={handleClick}>
+              Venue
+            </Link>
+          </li>
+          <li className="nav-item">
             <Link to="/contact" className="nav-links" onClick={handleClick}>
               Contact Us
             </Link>
