dd88641d6848347c7a71c2cec30a4c5e13d50621
Ashley936 <namitarastogimwn@gmail.com>
2022-04-12 20:47:59 +0530

    contact page edited

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Contact from "./Contact";
 
 function ContactPage() {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, []);
   return (
     <div className="contact-page">
       <div className="page-heading">
