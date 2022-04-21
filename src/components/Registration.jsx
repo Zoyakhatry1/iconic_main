6e9d92baafa1faf23240e36985b152effa6cb5d4
Zoyakhatry1 <83895340+Zoyakhatry1@users.noreply.github.com>
2022-04-21 23:38:50 +0530

    regchanges

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -14,77 +14,12 @@ function Registration() {
         <span>Registration</span>
       </div>
       <div className={classes.content}>
-        <div className={classes.text}>
-          <p>
-            At least one author of each accepted paper should get a full
-            registration.
-          </p>
-        </div>
-        <div className={classes.flex}>
-          <div className={classes.box1}>
-            <h2 className={classes.heading2}>Author</h2>
-            <p className={classes.text2}>
-              IEEE Member: <span className={classes.span}>$400.00</span>
-            </p>
-            <p className={classes.text2}>
-              Non Member: <span className={classes.span}>$480.00</span>
-            </p>
-            <p className={classes.text2}>
-              IEEE Life Member: <span className={classes.span}>$400.00</span>
-            </p>
-            <p className={classes.text2}>
-              IEEE Student Member:<span className={classes.span}>$400.00</span>
-            </p>
-            <p className={classes.text2}>
-              Student Non Member: <span className={classes.span}>$480.00</span>
-            </p>
-          </div>
-
-          <div className={classes.box2}>
-            <h2 className={classes.heading2}>Non-Author</h2>
-            <p className={classes.text2}>
-              IEEE Member: <span className={classes.span}>$400.00</span>
-            </p>
-            <p className={classes.text2}>
-              Non Member: <span className={classes.span}>$480.00</span>
-            </p>
-            <p className={classes.text2}>
-              IEEE Life Member: <span className={classes.span}>$400.00</span>
-            </p>
-            <p className={classes.text2}>
-              IEEE Student Member:<span className={classes.span}>$400.00</span>
-            </p>
-            <p className={classes.text2}>
-              Student Non Member:<span className={classes.span}>$480.00</span>
-            </p>
-          </div>
-        </div>
-        <div className={classes.center}>
-          <p className={classes.text3}>
-            The presenting author(s) must register to the conference before
-            submitting the final paper. Note that even if the presenter is a
-            student, he/she must pay a Full Registration. A paper not registered
-            by the deadline will not be included in the proceedings of the
-            conference. Every author can have an extra page at a cost of $100.00
-            each.
-          </p>
-          <p className={classes.text4}>
-            IMPORTANT NOTE: Payments using wire transfer require an extra fee
-            that is up to the author to pay. Authors who will use wire transfer
-            for a payment method are advised to email a copy of the receipt to
-            the Financial Char of the conference as well as make sure that the
-            transfer will be complete before the start date of the conference.
-            Any incomplete wire transfers will be subjected to removal of the
-            paper from the proceedings.
-          </p>
-        </div>
-
         <div className={classes.fee}>
           <h1 className={classes.headingfee}>Registration Fees</h1>
           <div className={classes.tablecontainer}>
             <table className={classes.table}>
               <tr>
-                <td rowSpan="2">
+                <td>
                   Student Registration (for Virtual Participation)
                 </td>
                 <td>IEEE/CSI Member</td>
