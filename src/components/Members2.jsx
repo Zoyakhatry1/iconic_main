ac670461916e8ac491ae35bc61fe75ae1460d956
Ashley936 <namitarastogimwn@gmail.com>
2022-06-02 16:51:56 +0530

    Tech Comitte

--- src/components/Members2.jsx
+++ src/components/Members2.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import classes from "../styles/Members2.module.css";
 import Card from "./Card";
 import dirimg from "../assets/director.jpg";
@@ -14,10 +14,7 @@ import org3 from "../assets/org3.jpg";
 import org4 from "../assets/org4.jpg";
 import org5 from "../assets/org5.jpg";
 
-
-
 function Members2() {
-
   useEffect(() => {
     window.scrollTo({
       top: 0,
@@ -36,10 +33,10 @@ function Members2() {
         <div className={classes.patron}>
           <Card>
             <div className={classes.container}>
-            <img className={classes.img} src={dirimg} alt="" />
+              <img className={classes.img} src={dirimg} alt="" />
             </div>
             <p className={classes.name}>
-               Prof(Dr.) Rakesh Sehgal (Director, NIT Srinagar, India)
+              Prof(Dr.) Rakesh Sehgal (Director, NIT Srinagar, India)
             </p>
           </Card>
 
@@ -49,7 +46,7 @@ function Members2() {
             </div>
             <p className={classes.name}>
               {" "}
-               Prof(Dr.) Manoj Singh Gaur (Director, IIT Jammu, India)
+              Prof(Dr.) Manoj Singh Gaur (Director, IIT Jammu, India)
             </p>
           </Card>
         </div>
@@ -58,11 +55,11 @@ function Members2() {
         </div>
         <div className={classes.patron}>
           <Card>
-           <div className={classes.container}> 
-             <img className={classes.img} src={hodimg} alt="" />
-           </div>
+            <div className={classes.container}>
+              <img className={classes.img} src={hodimg} alt="" />
+            </div>
             <p className={classes.name}>
-               Prof. Roohie Naaz Mir (HOD, CSE , NIT Srinagar, India)
+              Prof. Roohie Naaz Mir (HOD, CSE , NIT Srinagar, India)
             </p>
           </Card>
         </div>
@@ -73,34 +70,32 @@ function Members2() {
         <div className={classes.patron}>
           <Card>
             <div className={classes.container}>
-            <img className={classes.img} src={tech1} alt="" />
+              <img className={classes.img} src={tech1} alt="" />
             </div>
             <p className={classes.name}>
-             Dr. Shaima Qureshi (NIT Srinagar, India
+              Dr. Shaima Qureshi (NIT Srinagar, India
             </p>
           </Card>
           <Card>
-          <div className={classes.container}>
-            <img className={classes.img} src={tech2} alt="" />
-          </div>
+            <div className={classes.container}>
+              <img className={classes.img} src={tech2} alt="" />
+            </div>
+            <p className={classes.name}>Dr. Dharitri Rath (IIT Jammu, India)</p>
+          </Card>
+          <Card>
+            <div className={classes.container}>
+              <img className={classes.img} src={tech3} alt="" />
+            </div>
             <p className={classes.name}>
-             Dr. Dharitri Rath (IIT Jammu, India)
+              Dr. Badri N subudhi (IIT Jammu, India)
             </p>
           </Card>
           <Card>
-          <div className={classes.container}>
-            <img className={classes.img} src={tech3} alt="" />
-          </div>
+            <div className={classes.container}>
+              <img className={classes.img} src={tech4} alt="" />
+            </div>
             <p className={classes.name}>
-             Dr. Badri N subudhi (IIT Jammu, India)
-            </p>
-          </Card>
-          <Card>
-          <div className={classes.container}>
-            <img className={classes.img} src={tech4} alt="" />
-          </div>
-            <p className={classes.name}>
-             Dr. Vinit Jakhetiya (IIT Jammu, India)
+              Dr. Vinit Jakhetiya (IIT Jammu, India)
             </p>
           </Card>
         </div>
@@ -110,44 +105,43 @@ function Members2() {
         </div>
         <div className={classes.patron}>
           <Card>
-          <div className={classes.container}>
-            <img className={classes.img} src={org1} alt="" />
-          </div>
+            <div className={classes.container}>
+              <img className={classes.img} src={org1} alt="" />
+            </div>
             <p className={classes.name}>
-             Dr. Ranjeet Kumar Rout (NIT Srinagar, India)
+              Dr. Ranjeet Kumar Rout (NIT Srinagar, India)
             </p>
           </Card>
           <Card>
-
-          <div className={classes.container}>
-            <img className={classes.img} src={org2} alt="" />
-          </div>
+            <div className={classes.container}>
+              <img className={classes.img} src={org2} alt="" />
+            </div>
             <p className={classes.name}>
-             Dr. Sparsh Sharma (NIT Srinagar, India)
+              Dr. Sparsh Sharma (NIT Srinagar, India)
             </p>
           </Card>
           <Card>
-          <div className={classes.container}>
-            <img className={classes.img} src={org3} alt="" />
-          </div>
+            <div className={classes.container}>
+              <img className={classes.img} src={org3} alt="" />
+            </div>
             <p className={classes.name}>
-             Dr. Pramod Kumar Yadav (NIT Srinagar)
+              Dr. Pramod Kumar Yadav (NIT Srinagar)
             </p>
           </Card>
           <Card>
-          <div className={classes.container}>
-          <img className={classes.img} src={org4} alt="" />
-          </div>
+            <div className={classes.container}>
+              <img className={classes.img} src={org4} alt="" />
+            </div>
             <p className={classes.name}>
-             Dr. Veningston K (NIT Srinagar, India)
+              Dr. Veningston K (NIT Srinagar, India)
             </p>
           </Card>
           <Card>
-          <div className={classes.container}>
-            <img className={classes.img} src={org5} alt="" />
-          </div>
+            <div className={classes.container}>
+              <img className={classes.img} src={org5} alt="" />
+            </div>
             <p className={classes.name}>
-             Dr. Saiyed Umer (Aliah University, Kolkata, India)
+              Dr. Saiyed Umer (Aliah University, Kolkata, India)
             </p>
           </Card>
         </div>
@@ -162,27 +156,25 @@ function Members2() {
             <div className={classes.names}>
               <p className={classes.name2}>
                 {" "}
-                 Dr. Ranjeet Kumar Rout (NIT Srinagar, India)
+                Dr. Ranjeet Kumar Rout (NIT Srinagar, India)
               </p>
               <p className={classes.name2}>
-                 Dr. Saiyed Umer (Aliah University, Kolkata, India)
+                Dr. Saiyed Umer (Aliah University, Kolkata, India)
               </p>
             </div>
           </div>
 
           <div>
             <div className={classes.buttonbox}>
-              <button className={classes.shadowbutton}>
-                Publicity Chair
-              </button>
+              <button className={classes.shadowbutton}>Publicity Chair</button>
             </div>
             <div className={classes.names}>
               <p className={classes.name2}>
                 {" "}
-                 Dr. Pramod Kumar Yadav (NIT Srinagar, India)
+                Dr. Pramod Kumar Yadav (NIT Srinagar, India)
               </p>
               <p className={classes.name2}>
-               Dr. Veningston K (NIT Srinagar, India)
+                Dr. Veningston K (NIT Srinagar, India)
               </p>
             </div>
           </div>
@@ -196,40 +188,61 @@ function Members2() {
             <div className={classes.names}>
               <p className={classes.name2}>
                 {" "}
-                 Dr. Ranjeet Kumar Rout (NIT Srinagar, India)
+                Dr. Ranjeet Kumar Rout (NIT Srinagar, India)
               </p>
-              <p className={classes.name2}>- Dr. Sparsh Sharma (NIT Srinagar, India)</p>
               <p className={classes.name2}>
-              Dr. Sudhir (NIT Srinagar, India)
+                - Dr. Sparsh Sharma (NIT Srinagar, India)
               </p>
-              <p className={classes.name2}>- Dr. Saiyed Umer (NIT Srinagar, India)</p> 
+              <p className={classes.name2}>Dr. Sudhir (NIT Srinagar, India)</p>
+              <p className={classes.name2}>
+                - Dr. Saiyed Umer (NIT Srinagar, India)
+              </p>
             </div>
           </div>
-          <div>
-            <div className={classes.buttonbox}>
-              <button className={classes.shadowbutton}>
-                Local Organizing Committee
-              </button>
-            </div>
-            <div className={classes.names2}>
-               <div className={classes.margin}>
-               <p className={classes.name2}> Dr. Annu Priya</p>
-               <p className={classes.name2}>Dr. Surbi Sharma</p>
-               <p className={classes.name2}>Sania Iqbal</p>
-               <p className={classes.name2}>Sadia Showkat</p>
-               <p className={classes.name2}>Monica</p>
-               <p className={classes.name2}>Anam Iqbal</p>
-               <p className={classes.name2}>Tausees Iqbal</p>
-               </div>
-              <div >
-              <p className={classes.name2}>Mehraj Tali</p>
-              <p className={classes.name2}>Sameer Dixit</p>
-              <p className={classes.name2}>Imtiaz Ahmad</p>
-              <p className={classes.name2}>Manzoor Ahmad Lone</p>
-              <p className={classes.name2}>Aafaq Dar</p>
-              <p className={classes.name2}>Nazir Shabbir</p>
+          <div className={classes.flexfull}>
+            <div>
+              <div className={classes.buttonbox}>
+                <button className={classes.shadowbutton}>
+                  Local Organizing Committee
+                </button>
               </div>
-              
+              <div className={classes.names2}>
+                <div className={classes.margin}>
+                  <p className={classes.name2}> Dr. Annu Priya</p>
+                  <p className={classes.name2}>Dr. Surbi Sharma</p>
+                  <p className={classes.name2}>Sania Iqbal</p>
+                  <p className={classes.name2}>Sadia Showkat</p>
+                  <p className={classes.name2}>Monica</p>
+                  <p className={classes.name2}>Anam Iqbal</p>
+                  <p className={classes.name2}>Tausees Iqbal</p>
+                </div>
+                <div>
+                  <p className={classes.name2}>Mehraj Tali</p>
+                  <p className={classes.name2}>Sameer Dixit</p>
+                  <p className={classes.name2}>Imtiaz Ahmad</p>
+                  <p className={classes.name2}>Manzoor Ahmad Lone</p>
+                  <p className={classes.name2}>Aafaq Dar</p>
+                  <p className={classes.name2}>Nazir Shabbir</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className={classes.buttonbox}>
+                <button className={classes.shadowbutton}>
+                  Local Technical Committee
+                </button>
+              </div>
+              <div className={classes.names2}>
+                <div className={classes.margin}>
+                  <p className={classes.name2}>Mahin Sagotra</p>
+                  <p className={classes.name2}>Yavar Mushtaq</p>
+                  <p className={classes.name2}>Isha Tamarkar</p>
+                </div>
+                <div>
+                  <p className={classes.name2}>Zoya Khatry</p>
+                  <p className={classes.name2}>Namita Rastogi</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
